d095f67bb2b95fd2158b8b55f0e297706a795a75
Murilo Sanches <murilando@gmail.com>
2019-04-02 08:47:43 -0300

    animacoes de banner, transitions, triggers e state angular 2

--- instagram-clone/src/app/acesso/banner/banner.component.ts
+++ instagram-clone/src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
+import { style, trigger, state, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel', animate('2s ease-in'))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public estado: string = 'escondido';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toggleEstado(): void {
+    console.log(this.estado);
+    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel';
   }
 
 }
